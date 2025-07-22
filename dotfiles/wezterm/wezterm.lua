774bfbb6a4b52345adc6c55634bc8dd21e4bc149
MSHR-Dec <mshr.dec@whisffee.net>
2025-07-22 20:17:11 +0900

    fix: wezterm

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -4,13 +4,19 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+function merge_config(config, new_config)
+  for k, v in pairs(new_config) do
+    config[k] = v
+  end
+end
+
 -- This is where you actually apply your config choices
 use_ime = true
 
 config.color_scheme = 'Dracula+'
 config.color_schemes = { ['Dracula+'] = { background = '#2B2B2B' }}
 config.font = wezterm.font('JetBrains Mono')
-config.font_size = 10.0
+config.font_size = 12.0
 config.window_frame = {
   font = wezterm.font('JetBrains Mono', { italic = true }),
   active_titlebar_bg = '#2B2B2B',
@@ -37,6 +43,9 @@ config.keys = {
   { key = 'c', mods = 'ALT', action = wezterm.action.CopyTo 'Clipboard' },
   { key = 'v', mods = 'ALT', action = wezterm.action.PasteFrom 'Clipboard' },
 }
+
+local custom = require("/custom")
+merge_config(config, custom)
 
 -- and finally, return the configuration to wezterm
 return config
