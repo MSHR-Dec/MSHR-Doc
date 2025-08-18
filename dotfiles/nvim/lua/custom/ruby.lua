061bc77119d7fe2a549459d859d411e173e004d5
MSHR-Dec <mshr.dec@whisffee.net>
2025-08-18 10:36:52 +0900

    fix: ruby-lsp

--- dotfiles/nvim/lua/custom/ruby.lua
+++ dotfiles/nvim/lua/custom/ruby.lua
@@ -1,6 +1,11 @@
-vim.g.vista_executive_for = {
-  ruby = "coc"
-}
-vim.g.coc_user_config = {
-  ["solargraph.shell"] = "/bin/zsh",
-}
+require("lspconfig").ruby_lsp.setup({
+  capabilities = vim.lsp.protocol.make_client_capabilities(),
+  init_options = {
+    formatter = 'standard',
+    linters = { 'standard' },
+  },
+  cmd = { vim.env.HOME .. '/.rbenv/shims/ruby-lsp' },
+  on_attach = function(client, bufnr)
+    require("lspkeymap").on_attach(client, bufnr)
+  end,
+})
